195f0bf78b27d8112b7219f7db2ec5598fefe078
Mary Healy <maryhealy1978@gmail.com>
2019-03-19 11:44:30 +0100

    Updates

---
+++ precache-manifest.f2facd1056ab85897e65da2cf21e0e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "106656c6ab1910a129af",
+    "url": "/beer_with_food/static/css/main.4e395d5e.chunk.css"
+  },
+  {
+    "revision": "106656c6ab1910a129af",
+    "url": "/beer_with_food/static/js/main.106656c6.chunk.js"
+  },
+  {
+    "revision": "caf9dd51ed4337bc56e7",
+    "url": "/beer_with_food/static/js/runtime~main.caf9dd51.js"
+  },
+  {
+    "revision": "b1533ebdc5f5d1757f9d",
+    "url": "/beer_with_food/static/css/2.8282511f.chunk.css"
+  },
+  {
+    "revision": "b1533ebdc5f5d1757f9d",
+    "url": "/beer_with_food/static/js/2.b1533ebd.chunk.js"
+  },
+  {
+    "revision": "f0c597e357b72f3fba893e5701d03ae2",
+    "url": "/beer_with_food/index.html"
+  }
+];
